12012141769af70f6127c44cdd58b3b944bd4de6
Daniel Fekete <danieleff@gmail.com>
2018-09-02 17:27:06 +0200

    do not crash on compilation error

--- src/Compiler.tsx
+++ src/Compiler.tsx
@@ -1,11 +1,18 @@
 import * as ts from 'typescript';
 
-export function compileString<T>(sourceString: string): T {
-    const compileResult = ts.transpileModule(sourceString, {
-      compilerOptions: {module: ts.ModuleKind.CommonJS, jsx: ts.JsxEmit.React}
-    });
-  
-    const resultObject = eval("var exports = {}\n;" + compileResult.outputText + ";\n exports;");
-  
-    return resultObject;
+export function compileString<T>(sourceString: string): T | null {
+
+    try {
+
+        const compileResult = ts.transpileModule(sourceString, {
+          compilerOptions: {module: ts.ModuleKind.CommonJS, jsx: ts.JsxEmit.React}
+        });
+      
+        const resultObject = eval("var exports = {}\n;" + compileResult.outputText + ";\n exports;");
+    
+        return resultObject;
+    } catch(e) {
+      console.error(e);
+      return null;
+    }
 }
